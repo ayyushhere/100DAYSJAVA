701662bcd41efa9dc4c0ef18026b29615cd3c0f1
ayyushhere <ayushkumar0211a@gmail.com>
2025-02-07 23:59:40 +0530

    Ques no 1 complete

---
+++ DAY 1/palindrome.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class palindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        int rx = 0;
+
+        while (temp > 0) {
+            int ld = temp % 10;
+            rx = rx * 10 + ld;
+            temp /= 10;
+        }
+
+        return rx == x;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int x = sc.nextInt();
+        
+        // Create an object of Solution class
+        Solution solution = new Solution();
+        
+        // Call the instance method
+        boolean result = solution.isPalindrome(x);
+
+        // Print result
+        System.out.println("Is Palindrome? " + result);
+
+        sc.close(); // Close the scanner to prevent resource leak
+    }
+}
