1786de8ef751977e663423a80e080dc54163f8d4
ayyushhere <ayushkumar0211a@gmail.com>
2025-02-09 10:49:07 +0530

    Ques 2 updated

--- DAY 2/powerOftwo.java
+++ DAY 2/powerOftwo.java
@@ -1,5 +1,11 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        return n<1? false:(n&(n-1))==0?true:false;
-    }
+import java.util.Scanner;
+
+class powerOftwo{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the no:: ");
+    int n = sc.nextInt();
+    System.out.println(n<1? false:(n&(n-1))==0?"yes it is in power of two.":false);
+    sc.close();
+}
 }
