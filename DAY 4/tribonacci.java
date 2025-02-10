70b0d45612b383ff332249c46d687e06591b6417
ayyushhere <ayushkumar0211a@gmail.com>
2025-02-11 00:01:17 +0530

    update code of day 4

--- DAY 4/tribonacci.java
+++ DAY 4/tribonacci.java
@@ -1,4 +1,5 @@
 public class tribonacci {
+    
     class Solution {  
         public int tribonacci(int n) {  
             if (n == 0) {  
@@ -11,18 +12,30 @@ public class tribonacci {
             int a = 0; // T(0)  
             int b = 1; // T(1)  
             int c = 1; // T(2)  
-    
+        
             int ft = 0; // This will hold T(n)  
-    
+        
             // Start calculating from T(3) to T(n)  
             for (int i = 3; i <= n; i++) {  
                 ft = a + b + c;  // T(n) = T(n-1) + T(n-2) + T(n-3)  
                 a = b;           // Move to the next number  
                 b = c;           // Move to the next number  
-                c = ft;         // Update the most recent number  
+                c = ft;          // Update the most recent number  
             }  
-    
+        
             return ft;  // Return the nth tribonacci number  
         }  
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Tribonacci class to access the Solution class
+        tribonacci tribonacciInstance = new tribonacci();
+        
+        // Create an instance of Solution class
+        Solution solution = tribonacciInstance.new Solution();
+        
+        // Call tribonacci method and print the result for a given n (e.g., n = 5)
+        int result = solution.tribonacci(5);  
+        System.out.println("The 5th Tribonacci number is: " + result);  // Should print 7
     }
 }
